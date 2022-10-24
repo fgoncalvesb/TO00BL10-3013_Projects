32709f146ba6dc4963bb499baeff1ebdc7333279
fgoncalvesb <40902412+fgoncalvesb@users.noreply.github.com>
2022-10-24 20:43:04 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 function checkFormInput() {
     
+    // I save into variables each part of the form that I want to validate
     var x = document.forms["myForm"]["email"].value;
     var y = document.forms["myForm"]["password"].value;
 
+    // Conditions that I want to validate for email
     if ((x == null || x == "") || (x.length < 3 || x.includes("@")==false)) {
+    
+    // When conditions are not met, I want the borders of the field to become red, to select the text and focus on the field.
     document.forms.myForm.email.style.borderColor = "red";
     document.forms.myForm.email.select();  
     document.forms.myForm.email.focus(); 
+
+    // In the "span" part of the label, I want for a message to appear if validation is not passed, together with an alert message
     document.getElementById('emailFeedback').innerHTML="<b>*Fill in properly</b>";
     alert("Email must be properly filled out");
 
@@ -14,6 +20,7 @@ function checkFormInput() {
     return false;
     }
 
+    // Conditions that I want to validate for password
     if (y == null || y == "") {
         document.forms.myForm.password.style.borderColor = "red";
         alert("Password must be properly filled out");
@@ -21,6 +28,7 @@ function checkFormInput() {
         return false;
         }
 
+    // Finally, validation not for format but for correct username and password
     if (x == 'admin@admin.com' && y == 'admin'){
         window.location.replace("ToDoList.html");
         return false;
